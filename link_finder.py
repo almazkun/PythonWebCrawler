0113cdd1a4f5792ad7dfafcde2b35c115b7cb37e
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-12-02 21:49:33 +0600

    HTML parser mod

---
+++ link_finder.py
@@ -0,0 +1,19 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+
+    def error(self, message):
+        pass
+
+
+finder = LinkFinder()
+finder.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me!</h1></body></html>')
